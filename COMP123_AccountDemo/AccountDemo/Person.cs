803c9b12e78770a4ce94a2700e8ef3d2da307e73
LightCelesteFlare <Brandonkool16@gmail.com>
2017-03-22 12:03:39 -0400

    Add files via upload

--- COMP123_AccountDemo/AccountDemo/Person.cs
+++ COMP123_AccountDemo/AccountDemo/Person.cs
@@ -8,5 +8,46 @@ namespace Assignment3
 {
     class Person
     {
+        // Private fields
+        private string password;
+
+        // Public fields
+        public string SIN;
+
+        // Properties
+        public bool IsAuthenticated { get; private set; }
+        public string Name { get; private set; }
+
+        // Methods
+        public Person(string name, string sin)
+        {
+            SIN = sin;
+            Name = name;
+
+            
+        }
+
+        public void Login(string password)
+        {
+            this.password = password;
+            if (Account.password != password)
+            {
+                throw new AccountException(AccountException.IsAuthenticated);
+            }
+            else
+            {
+                IsAuthenticated = true;
+            }
+        }
+
+        public void Logout()
+        {
+            IsAuthenticated = false;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} is {1}", Name, IsAuthenticated ? "authenticated" : "Not authenticated");
+        }
     }
 }
