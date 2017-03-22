3e034481ee381489e1d580c29aacd3c5dda5d561
Daniel Stokes <danielphilipstokes@gmail.com>
2017-03-22 11:28:54 -0400

    Add files via upload

---
+++ COMP123_AccountDemo/AccountDemo/Account.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountDemo
+{
+    abstract class Account
+    {
+        //fields
+        public readonly List<Person> holders = new List<Person>();
+        public readonly List<Transactions> transactions = new List<Transactions>();
+        public readonly string Number;
+        private int LAST_NUMBER;
+
+        //properties
+        public double Balance { get; protected set; }
+        public double LowestBalance { get; protected set; }
+
+        public Account(string type, double balance) {
+            Balance = balance;
+            LowestBalance = balance;
+
+            Number = type + LAST_NUMBER.ToString();
+            LAST_NUMBER++;
+        }
+
+        public void AddUser(Person person) {
+            holders.Add(person);
+        }
+
+        public Deposit(double amount, Person person){
+            Balance +=amount;
+            LowestBalance = Balance;
+
+            Transaction transaction = new Transaction(DateTime.Now, Number, amount, person, Balance);
+            transactions.Add(transaction);
+
+        }
+
+        public bool IsHolder(string name){
+            foreach(Person p in holders){
+                if(p.name = name){
+                    return true;
+                }
+            }
+        }
+
+        public abstract void PrepareMonthlyReport(){}
+
+        public override string ToString() {
+            string output = "";
+            output += Number;
+            foreach(Person p in holders){
+                output += p.name;
+            }
+            output +=Balance;
+
+            foreach(Transaction t in transactions){
+                output+=t;
+            }
+
+            return output;
+        }
+
+    }
+}
