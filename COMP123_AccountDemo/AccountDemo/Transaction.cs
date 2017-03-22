0f77a4273bfd6f65013abe5101c0e93aeedc8ef3
Daniel Stokes <danielphilipstokes@gmail.com>
2017-03-22 11:41:16 -0400

    Add files via upload

---
+++ COMP123_AccountDemo/AccountDemo/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountDemo
+{
+    class Transaction
+    {
+        public readonly string AccountNumber;
+        public readonly double Amount;
+        public readonly double EndBalance;
+        public readonly Person Originator;
+        public readonly DateTime Time;
+
+        public Transaction(string accountNumber, double amount, double endBalance, Person person, DateTime time) {
+            AccountNumber = accountNumber;
+            Amount = amount;
+            EndBalance = endBalance;
+            Originator = person;
+            Time = time;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Account Number: {0}. {1}{2}. Balance after transaction: {3}. Person performing transaction: {4}. Time of transaction: {5}", AccountNumber, Amount < 0 ? "Withdraw amount:" : "Deposit amount", Amount, EndBalance, person, Time.ToShortTimeString );
+        }
+
+
+    }
+}
