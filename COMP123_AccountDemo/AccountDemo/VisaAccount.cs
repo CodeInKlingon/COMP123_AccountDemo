d5510b525047ff244c0d802f16f79bdd27831c62
Daniel Stokes <danielphilipstokes@gmail.com>
2017-03-22 11:32:38 -0400

    Add files via upload

---
+++ COMP123_AccountDemo/AccountDemo/VisaAccount.cs
@@ -0,0 +1,55 @@
+﻿//Devin Young
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountDemo
+{
+    class VisaAccount : Account
+    {
+        private double creditLimit;
+        public double INTEREST_RATE = 0.1995;
+
+        public VisaAccount(double balance = 0, double creditLimit = 1200)
+            :base ("VS-", balance)
+        {
+            this.creditLimit = creditLimit;
+        }
+
+        public void DoPayment(double amount, Person person)
+        {
+            base.Deposit(amount, person);
+        }
+
+        public void DoPurchase(double amount, Person person)
+        {
+            if (!base.holders.Contains(person))
+            {
+                throw AccountException(AccountException.ACCOUNT_DOES_NOT_EXIST);
+            }
+            else if (!person.IsAuthenticated)
+            {
+                throw AccountException(AccountException.USER_NOT_LOGGED_IN);
+            }
+            else if (base.Balance < amount)
+            {
+                throw AccountException(AccountException.CREDIT_LIMIT_HAS_BEEN_EXCEEDED);
+            }
+            else
+            {
+                base.Deposit(amount, person);
+            }
+        }
+        public override void PrepareMonthlyReport()
+        {
+            INTEREST_RATE = (LowestBalance * INTEREST_RATE) / 12;
+
+            Balance -= INTEREST_RATE;
+
+            transactions = new List<Transactions>();
+        }
+        
+    }
+}
